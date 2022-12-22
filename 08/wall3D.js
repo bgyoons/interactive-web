ba8da60713eda884d1726b6f76729f63373da0af
bgyoonseo <73841260+bgyoons@users.noreply.github.com>
2022-12-22 11:27:14 +0900

    08 add progress bar

--- 08/wall3D.js
+++ 08/wall3D.js
@@ -1,5 +1,8 @@
 (function () {
-  const houseEle = document.querySelector('.house')
+  const houseEle = document.querySelector('.house');
+  const stageEle = document.querySelector('.stage');
+  const barEle = document.querySelector('.progress-bar');
+  const mousePosition = {x: 0, y: 0};
   let maxScrollValue;
   
   function resizeHandler() {
@@ -7,8 +10,16 @@
   }
 
   window.addEventListener('scroll', function() {
-    const zMove = this.scrollY / maxScrollValue * 980;
+    const scrollPercent = this.scrollY / maxScrollValue;
+    const zMove = scrollPercent * 980;
     houseEle.style.transform = `translateZ(${zMove - 490}vw)`
+    barEle.style.width = `${scrollPercent * 100}%`
+  })
+
+  window.addEventListener('mousemove', function(e) {
+    mousePosition.x = -1 + e.clientX / window.innerWidth * 2;
+    mousePosition.y = 1 - e.clientY / window.innerHeight * 2;
+    stageEle.style.transform = `rotateX(${mousePosition.y * 5}deg) rotateY(${mousePosition.x * 5}deg)`
   })
 
   window.addEventListener('resize', resizeHandler)
